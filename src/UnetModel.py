a41e28f668090ae5741396f993f5de7a51e4d0e2
Carlosbox2k <carborort@alum.us.es>
2026-05-25 15:26:04 +0200

    feat: modelo U-Net v1

---
+++ src/UnetModel.py
@@ -0,0 +1,43 @@
+from keras.layers import Conv2D, Input, MaxPooling2D, Conv2DTranspose, concatenate
+from keras.models import Model
+import tensorflow as tf
+import os
+
+HORIZONTAL_SIZE = int(os.getenv("HORIZONTAL_SIZE", 565))
+VERTICAL_SIZE = int(os.getenv("VERTICAL_SIZE", 584))
+
+def conv_block(input, num_filters):
+    conv = Conv2D(num_filters, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(input)
+    conv = Conv2D(num_filters, (3, 3), activation="relu", padding="same", kernel_initializer='he_normal')(conv)
+    return conv
+
+def encoder_block(input, num_filters):
+    conv = conv_block(input, num_filters)
+    pool = MaxPooling2D((2, 2))(conv)
+    return conv, pool
+
+def decoder_block(input, skip_features, num_filters):
+    uconv = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    con = concatenate([uconv, skip_features])
+    conv = conv_block(con, num_filters)
+    return conv
+
+def build_model(input_shape):
+    input_layer = Input(input_shape)
+    
+    s1, p1 = encoder_block(input_layer, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
+
+    b1 = conv_block(p4, 1024)
+
+    d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(d1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
+    d4 = decoder_block(d3, s1, 64)
+    
+    output_layer = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
+    
+    model = Model(input_layer, output_layer, name="U-Net")
+    return model

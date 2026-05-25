7dddb5d46ba36344192623df67637d05e123f27c
Carlosbox2k <carborort@alum.us.es>
2026-05-25 15:24:43 +0200

    feat: tamaño horizontal y vertical  de las imágenes y máscaras como variables globales en el .env para poder cambiar su valor fácilmente

--- src/DataGenerator.py
+++ src/DataGenerator.py
@@ -1,30 +1,29 @@
 import tensorflow as tf
 from glob import glob
 import numpy as np
-from sklearn.model_selection import train_test_split
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib
 from keras.models import Model
-from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate
 from keras.optimizers import Adam
-#from tensorflow.keras.metrics import *
 import os
 
 
 ROOT_PATH = os.path.abspath("data")
 TEST_PATH = os.path.join(ROOT_PATH, "test")
 TRAIN_PATH = os.path.join(ROOT_PATH, "training")
+HORIZONTAL_SIZE = int(os.getenv("HORIZONTAL_SIZE", 565))
+VERTICAL_SIZE = int(os.getenv("VERTICAL_SIZE", 584))
 
 training_images_path = os.path.join(TRAIN_PATH, "images")
 training_masks_path = os.path.join(TRAIN_PATH, "mask")
+
 print(f"Training images path: {training_images_path}")
+
 def load_image(path):
     print(f"Loading image from: {path}")
-    # Usar np.fromfile y cv2.imdecode porque cv2.imread falla con caracteres especiales en la ruta (como "3º")
     image = cv2.imread(path)
-    
-    # image = cv2.resize(image, (size,size))
+    image = cv2.resize(image, (HORIZONTAL_SIZE, VERTICAL_SIZE))
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)   # shape: (size,size,3) -> (size,size,1)
     image = image/255.   # normalize
     return image
@@ -41,7 +40,6 @@ def load_data(images_path, masks_path):
         masks.append(load_image(masks_paths[i]))
     
     return np.array(images), np.array(masks)
-
 
 X, y = load_data(training_images_path, training_masks_path)
 print(f"X shape: {X.shape}")
